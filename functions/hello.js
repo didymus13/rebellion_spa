e2d50ab0a63c7481ca4c99589ba157449f1e4beb
Stephane Doiron <stephane.doiron@b367.ca>
2020-02-18 16:04:12 -0500

    work dammit!

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
-exports.handler = (event, ctx) => {
-  return {
+exports.handler = (event, ctx, callback) => {
+  return callback(null, {
     statusCode: 200,
     body: 'Foo Bar Baz'
-  }
+  })
 }
