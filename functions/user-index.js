a3cbaae06b063c174b9db4f72a1cb4f128c8ad44
Stephane Doiron <stephane.doiron@b367.ca>
2020-02-18 16:27:47 -0500

    user list 1st try

---
+++ functions/user-index.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import User from '../models/User'
+
+mongoose.connect(process.env.DB_DSN, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+
+exports.handler = async (event, ctx) => {
+  const users = await User.find({})
+  return {
+    statusCode: 200,
+    body: JSON.stringify(users)
+  }
+}
---
+++ models/User.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true }
+})
+export default mongoose.model('User', UserSchema)
