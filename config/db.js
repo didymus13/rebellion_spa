d60dfaae3610ad86d44e953e849a8d3f57313290
Stephane Doiron <stephane.doiron@b367.ca>
2020-02-18 16:01:05 -0500

    Scale back tests

--- config/db.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-mongoose.connect(process.env.DB_DSN, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
---
+++ functions/hello.js
@@ -0,0 +1,6 @@
+exports.handler = (event, ctx) => {
+  return {
+    statusCode: 200,
+    body: 'Foo Bar Baz'
+  }
+}
